5b72e5a72c4e632d7a0816cca14681fdd04c915a
tlaektmf <tlaektmf0826@gmail.com>
2019-07-23 08:23:03 +0900

    0722

---
+++ glove.py
@@ -0,0 +1,426 @@
+# import re, gzip, pickle, time
+# from multiprocessing import Queue, Lock
+# import threading
+# import numpy as np
+# import pyximport
+# pyximport.install(setup_args={"include_dirs": np.get_include()})
+#
+# from .glove_inner import train_glove
+#
+# class Glove(object):
+#     def __init__(self, cooccurence, alpha=0.75, x_max=100.0, d=50, seed=1234):
+#         """
+#         Glove model for obtaining dense embeddings from a
+#         co-occurence (sparse) matrix.
+#         """
+#         self.alpha           = alpha
+#         self.x_max           = x_max
+#         self.d               = d
+#         self.cooccurence     = cooccurence
+#         self.seed            = seed
+#         np.random.seed(seed)
+#         self.W               = np.random.uniform(-0.5/d, 0.5/d, (len(cooccurence), d)).astype(np.float64)
+#         self.ContextW        = np.random.uniform(-0.5/d, 0.5/d, (len(cooccurence), d)).astype(np.float64)
+#         self.b               = np.random.uniform(-0.5/d, 0.5/d, (len(cooccurence), 1)).astype(np.float64)
+#         self.ContextB        = np.random.uniform(-0.5/d, 0.5/d, (len(cooccurence), 1)).astype(np.float64)
+#         self.gradsqW         = np.ones_like(self.W, dtype=np.float64)
+#         self.gradsqContextW  = np.ones_like(self.ContextW, dtype=np.float64)
+#         self.gradsqb         = np.ones_like(self.b, dtype=np.float64)
+#         self.gradsqContextB  = np.ones_like(self.ContextB, dtype=np.float64)
+#
+#     def train(self, step_size=0.05, workers = 9, batch_size=50, verbose=False):
+#         jobs = Queue(maxsize=2 * workers)
+#         lock = threading.Lock()  # for shared state (=number of words trained so far, log reports...)
+#         total_error = [0.0]
+#         total_done  = [0]
+#
+#         total_els = 0
+#         for key in self.cooccurence:
+#             for subkey in self.cooccurence[key]:
+#                 total_els += 1
+#
+#         # Worker function:
+#         def worker_train():
+#             error = np.zeros(1, dtype = np.float64)
+#             while True:
+#                 job = jobs.get()
+#                 if job is None:  # data finished, exit
+#                     break
+#                 train_glove(self, job, step_size, error)
+#                 with lock:
+#                     total_error[0] += error[0]
+#                     total_done[0] += len(job[0])
+#                     if verbose:
+#                         if total_done[0] % 1000 == 0:
+#                             print("Completed %.3f%%\r" % (100.0 * total_done[0] / total_els))
+#                 error[0] = 0.0
+#
+#         # Create workers
+#         workers_threads = [threading.Thread(target=worker_train) for _ in range(workers)]
+#         for thread in workers_threads:
+#             thread.daemon = True  # make interrupting the process with ctrl+c easier
+#             thread.start()
+#
+#         # Batch co-occurence pieces
+#         batch_length = 0
+#         batch = []
+#         num_examples = 0
+#         for key in self.cooccurence:
+#             for subkey in self.cooccurence[key]:
+#                 batch.append((key, subkey, self.cooccurence[key][subkey]))
+#                 batch_length += 1
+#                 if batch_length >= batch_size:
+#                     jobs.put(
+#                         (
+#                             np.array([k for k,s,c in batch], dtype=np.int32),
+#                             np.array([s for k,s,c in batch], dtype=np.int32),
+#                             np.array([c for k,s,c in batch], dtype=np.float64)
+#                         )
+#                     )
+#                     num_examples += len(batch)
+#                     batch = []
+#                     batch_length = 0
+#         if len(batch) > 0:
+#             jobs.put(
+#                 (
+#                     np.array([k for k,s,c in batch], dtype=np.int32),
+#                     np.array([s for k,s,c in batch], dtype=np.int32),
+#                     np.array([c for k,s,c in batch], dtype=np.float64)
+#                 )
+#             )
+#             num_examples += len(batch)
+#             batch = []
+#             batch_length = 0
+#
+#         for _ in range(workers):
+#             jobs.put(None)  # give the workers heads up that they can finish -- no more work!
+#
+#         for thread in workers_threads:
+#             thread.join()
+#
+#         return total_error[0] / num_examples
+
+
+##수정
+import array
+import collections
+import io
+try:
+    # Python 2 compat
+    import cPickle as pickle
+except ImportError:
+    import pickle
+
+import numpy as np
+import scipy.sparse as sp
+import numbers
+import pyximport; pyximport.install()
+from .glove_cython import fit_vectors, transform_paragraph
+
+
+def check_random_state(seed):
+    """ Turn seed into a np.random.RandomState instance.
+        This is a copy of the check_random_state function in sklearn
+        in order to avoid outside dependencies.
+    """
+    if seed is None or seed is np.random:
+        return np.random.mtrand._rand
+    if isinstance(seed, (numbers.Integral, np.integer)):
+        return np.random.RandomState(seed)
+    if isinstance(seed, np.random.RandomState):
+        return seed
+    raise ValueError('%r cannot be used to seed a numpy.random.RandomState'
+                     ' instance' % seed)
+
+
+class Glove(object):
+    """
+    Class for estimating GloVe word embeddings using the
+    corpus coocurrence matrix.
+    """
+
+    def __init__(self, no_components=30, learning_rate=0.05,
+                 alpha=0.75, max_count=100, max_loss=10.0,
+                 random_state=None):
+        """
+        Parameters:
+        - int no_components: number of latent dimensions
+        - float learning_rate: learning rate for SGD estimation.
+        - float alpha, float max_count: parameters for the
+          weighting function (see the paper).
+        - float max_loss: the maximum absolute value of calculated
+                          gradient for any single co-occurrence pair.
+                          Only try setting to a lower value if you
+                          are experiencing problems with numerical
+                          stability.
+        - random_state: random statue used to intialize optimization
+        """
+
+        self.no_components = no_components
+        self.learning_rate = float(learning_rate)
+        self.alpha = float(alpha)
+        self.max_count = float(max_count)
+        self.max_loss = max_loss
+
+        self.word_vectors = None
+        self.word_biases = None
+
+        self.vectors_sum_gradients = None
+        self.biases_sum_gradients = None
+
+        self.dictionary = None
+        self.inverse_dictionary = None
+
+        self.random_state = random_state
+
+    def fit(self, matrix, epochs=5, no_threads=2, verbose=False):
+        """
+        Estimate the word embeddings.
+        Parameters:
+        - scipy.sparse.coo_matrix matrix: coocurrence matrix
+        - int epochs: number of training epochs
+        - int no_threads: number of training threads
+        - bool verbose: print progress messages if True
+        """
+
+        shape = matrix.shape
+
+        if (len(shape) != 2 or
+            shape[0] != shape[1]):
+            raise Exception('Coocurrence matrix must be square')
+
+        if not sp.isspmatrix_coo(matrix):
+            raise Exception('Coocurrence matrix must be in the COO format')
+
+        random_state = check_random_state(self.random_state)
+        self.word_vectors = ((random_state.rand(shape[0],
+                                                self.no_components) - 0.5)
+                             / self.no_components)
+        self.word_biases = np.zeros(shape[0],
+                                    dtype=np.float64)
+
+        self.vectors_sum_gradients = np.ones_like(self.word_vectors)
+        self.biases_sum_gradients = np.ones_like(self.word_biases)
+
+        shuffle_indices = np.arange(matrix.nnz, dtype=np.int32)
+
+        if verbose:
+            print('Performing %s training epochs '
+                  'with %s threads' % (epochs, no_threads))
+
+        for epoch in range(epochs):
+
+            if verbose:
+                print('Epoch %s' % epoch)
+
+            # Shuffle the coocurrence matrix
+            random_state.shuffle(shuffle_indices)
+
+            fit_vectors(self.word_vectors,
+                        self.vectors_sum_gradients,
+                        self.word_biases,
+                        self.biases_sum_gradients,
+                        matrix.row,
+                        matrix.col,
+                        matrix.data,
+                        shuffle_indices,
+                        self.learning_rate,
+                        self.max_count,
+                        self.alpha,
+                        self.max_loss,
+                        int(no_threads))
+
+            if not np.isfinite(self.word_vectors).all():
+                raise Exception('Non-finite values in word vectors. '
+                                'Try reducing the learning rate or the '
+                                'max_loss parameter.')
+
+    def transform_paragraph(self, paragraph, epochs=50, ignore_missing=False):
+        """
+        Transform an iterable of tokens into its vector representation
+        (a paragraph vector).
+        Experimental. This will return something close to a tf-idf
+        weighted average of constituent token vectors by fitting
+        rare words (with low word bias values) more closely.
+        """
+
+        if self.word_vectors is None:
+            raise Exception('Model must be fit to transform paragraphs')
+
+        if self.dictionary is None:
+            raise Exception('Dictionary must be provided to '
+                            'transform paragraphs')
+
+        cooccurrence = collections.defaultdict(lambda: 0.0)
+
+        for token in paragraph:
+            try:
+                cooccurrence[self.dictionary[token]] += self.max_count / 10.0
+            except KeyError:
+                if not ignore_missing:
+                    raise
+
+        random_state = check_random_state(self.random_state)
+
+        word_ids = np.array(cooccurrence.keys(), dtype=np.int32)
+        values = np.array(cooccurrence.values(), dtype=np.float64)
+        shuffle_indices = np.arange(len(word_ids), dtype=np.int32)
+
+        # Initialize the vector to mean of constituent word vectors
+        paragraph_vector = np.mean(self.word_vectors[word_ids], axis=0)
+        sum_gradients = np.ones_like(paragraph_vector)
+
+        # Shuffle the coocurrence matrix
+        random_state.shuffle(shuffle_indices)
+        transform_paragraph(self.word_vectors,
+                            self.word_biases,
+                            paragraph_vector,
+                            sum_gradients,
+                            word_ids,
+                            values,
+                            shuffle_indices,
+                            self.learning_rate,
+                            self.max_count,
+                            self.alpha,
+                            epochs)
+
+        return paragraph_vector
+
+    def add_dictionary(self, dictionary):
+        """
+        Supply a word-id dictionary to allow similarity queries.
+        """
+
+        if self.word_vectors is None:
+            raise Exception('Model must be fit before adding a dictionary')
+
+        if len(dictionary) > self.word_vectors.shape[0]:
+            raise Exception('Dictionary length must be smaller '
+                            'or equal to the number of word vectors')
+
+        self.dictionary = dictionary
+        if hasattr(self.dictionary, 'iteritems'):
+            # Python 2 compat
+            items_iterator = self.dictionary.iteritems()
+        else:
+            items_iterator = self.dictionary.items()
+
+        self.inverse_dictionary = {v: k for k, v in items_iterator}
+
+    def save(self, filename):
+        """
+        Serialize model to filename.
+        """
+
+        with open(filename, 'wb') as savefile:
+            pickle.dump(self.__dict__,
+                        savefile,
+                        protocol=pickle.HIGHEST_PROTOCOL)
+
+    @classmethod
+    def load(cls, filename):
+        """
+        Load model from filename.
+        """
+
+        instance = Glove()
+
+        with open(filename, 'rb') as savefile:
+            instance.__dict__ = pickle.load(savefile)
+
+        return instance
+
+    @classmethod
+    def load_stanford(cls, filename):
+        """
+        Load model from the output files generated by
+        the C code from http://nlp.stanford.edu/projects/glove/.
+        The entries of the word dictionary will be of type
+        unicode in Python 2 and str in Python 3.
+        """
+
+        dct = {}
+        vectors = array.array('d')
+
+        # Read in the data.
+        with io.open(filename, 'r', encoding='utf-8') as savefile:
+            for i, line in enumerate(savefile):
+                tokens = line.split(' ')
+
+                word = tokens[0]
+                entries = tokens[1:]
+
+                dct[word] = i
+                vectors.extend(float(x) for x in entries)
+
+        # Infer word vectors dimensions.
+        no_components = len(entries)
+        no_vectors = len(dct)
+
+        # Set up the model instance.
+        instance = Glove()
+        instance.no_components = no_components
+        instance.word_vectors = (np.array(vectors)
+                                 .reshape(no_vectors,
+                                          no_components))
+        instance.word_biases = np.zeros(no_vectors)
+        instance.add_dictionary(dct)
+
+        return instance
+
+    def _similarity_query(self, word_vec, number):
+
+        dst = (np.dot(self.word_vectors, word_vec)
+               / np.linalg.norm(self.word_vectors, axis=1)
+               / np.linalg.norm(word_vec))
+        word_ids = np.argsort(-dst)
+
+        return [(self.inverse_dictionary[x], dst[x]) for x in word_ids[:number]
+                if x in self.inverse_dictionary]
+
+    def most_similar(self, word, number=5):
+        """
+        Run a similarity query, retrieving number
+        most similar words.
+        """
+
+        if self.word_vectors is None:
+            raise Exception('Model must be fit before querying')
+
+        if self.dictionary is None:
+            raise Exception('No word dictionary supplied')
+
+        try:
+            word_idx = self.dictionary[word]
+        except KeyError:
+            raise Exception('Word not in dictionary')
+
+        return self._similarity_query(self.word_vectors[word_idx], number)[1:]
+
+####################################
+    def sim_value(self,word1,word2):
+        "다슬"
+        try:
+            word_idx1 = self.dictionary[word1]
+            word_idx2=self.dictionary[word2]
+
+        except KeyError:
+            print('Word not in dictionary')
+            # raise Exception('Word not in dictionary')
+            return -1
+
+        dst = (np.dot(self.word_vectors[word_idx1], self.word_vectors[word_idx2])
+               / np.linalg.norm(self.word_vectors[word_idx1])
+               / np.linalg.norm(self.word_vectors[word_idx2]))
+
+        return dst # 유사도 반환
+####################################
+
+    def most_similar_paragraph(self, paragraph, number=5, **kwargs):
+        """
+        Return words most similar to a given paragraph (iterable of tokens).
+        """
+
+        paragraph_vector = self.transform_paragraph(paragraph, **kwargs)
+
+        return self._similarity_query(paragraph_vector, number)
